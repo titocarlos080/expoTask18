cd67b7feef02610b0ee0d90e8d76369475237b3b
Tito Carlos <titocarlos080@gmail.com>
2024-06-04 08:29:44 -0400

    error corregido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './task/components/navigation/navigation.component';
+import { TaskPageComponent } from './task/pages/task-page/task-page.component';
   
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet ,NavigationComponent],
+  imports: [RouterOutlet ,NavigationComponent,TaskPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
